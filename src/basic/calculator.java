bce61943f3d07e406fd990559e588dd9ebcf9b22
DivyaMacharla123 <divya.macharla@worldline.com>
2022-08-18 17:36:24 +0530

    added math and calculator

--- src/basic/calculator.java
+++ src/basic/calculator.java
@@ -1,5 +1,13 @@
 package basic;
 
 public class calculator {
+	public static void main(String[] args) {
+		System.out.println("addition");
+		Math.add(22,67);
+		Math.add(65,89,43);
+		System.out.println("multiplication");
+		Math.multiply(23,8);
+		
+	}
 
 }
